813dfb90614f0af24e6c246ab56c3eef5eef2f01
The Underdark <under@dark.com>
2023-01-03 10:48:23 +1100

    Add project files.

---
+++ XpEditorForSynthesis/Settings.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XpEditorForSynthesis
+{
+    public class Settings
+    {
+        public float AlterationSkillUseMult = 3;
+        public float AlterationSkillUseOffset = 0;
+        public float AlterationSkillImproveMult = 2;
+        public float AlterationSkillImproveOffset = 0;
+
+        public float ConjurationSkillUseMult = 2.1f;
+        public float ConjurationSkillUseOffset = 0;
+        public float ConjurationSkillImproveMult = 2;
+        public float ConjurationSkillImproveOffset = 0;
+
+        public float DestructionSkillUseMult = 1.35f;
+        public float DestructionSkillUseOffset = 0;
+        public float DestructionSkillImproveMult = 2;
+        public float DestructionSkillImproveOffset = 0;
+
+        public float IllusionSkillUseMult = 4.6f;
+        public float IllusionSkillUseOffset = 0;
+        public float IllusionSkillImproveMult = 2;
+        public float IllusionSkillImproveOffset = 0;
+
+        public float RestorationSkillUseMult = 2;
+        public float RestorationSkillUseOffset = 0;
+        public float RestorationSkillImproveMult = 2;
+        public float RestorationSkillImproveOffset = 0;
+
+        public float EnchantingSkillUseMult = 900;
+        public float EnchantingSkillUseOffset = 0;
+        public float EnchantingSkillImproveMult = 1;
+        public float EnchantingSkillImproveOffset = 170;
+
+        public float OneHandedSkillUseMult = 6.3f;
+        public float OneHandedSkillUseOffset = 0;
+        public float OneHandedSkillImproveMult = 2;
+        public float OneHandedSkillImproveOffset =0;
+
+        public float TwoHandedSkillUseMult = 5.95f;
+        public float TwoHandedSkillUseOffset =0;
+        public float TwoHandedSkillImproveMult = 2;
+        public float TwoHandedSkillImproveOffset =0;
+
+        public float ArcherySkillUseMult = 9.3f;
+        public float ArcherySkillUseOffset = 0;
+        public float ArcherySkillImproveMult = 2;
+        public float ArcherySkillImproveOffset =0 ;
+
+        public float BlockSkillUseMult = 8.1f;
+        public float BlockSkillUseOffset = 0;
+        public float BlockSkillImproveMult = 2;
+        public float BlockSkillImproveOffset = 0;
+
+        public float SmithingSkillUseMult = 1;
+        public float SmithingSkillUseOffset = 0;
+        public float SmithingSkillImproveMult =0.25f;
+        public float SmithingSkillImproveOffset =300;
+
+        public float HeavyArmorSkillUseMult = 3.8f;
+        public float HeavyArmorSkillUseOffset = 0;
+        public float HeavyArmorSkillImproveMult = 2;
+        public float HeavyArmorSkillImproveOffset = 0;
+
+        public float LightArmorSkillUseMult = 4;
+        public float LightArmorSkillUseOffset = 0;
+        public float LightArmorSkillImproveMult = 2;
+        public float LightArmorSkillImproveOffset = 0; 
+
+        public float PickpocketSkillUseMult = 8.1f;
+        public float PickpocketSkillUseOffset = 0;
+        public float PickpocketSkillImproveMult = 0.25f;
+        public float PickpocketSkillImproveOffset = 300;
+
+        public float LockpickingSkillUseMult = 45;
+        public float LockpickingSkillUseOffset = 10;
+        public float LockpickingSkillImproveMult = 0.25f;
+        public float LockpickingSkillImproveOffset = 300;
+
+        public float SneakSkillUseMult = 11.25f;
+        public float SneakSkillUseOffset = 0;
+        public float SneakSkillImproveMult = 0.5f;
+        public float SneakSkillImproveOffset = 120;
+
+        public float AlchemySkillUseMult = 0.75f;
+        public float AlchemySkillUseOffset = 0;
+        public float AlchemySkillImproveMult = 1.6f;
+        public float AlchemySkillImproveOffset = 65;
+
+        public float SpeechSkillUseMult = 0.36f;
+        public float SpeechSkillUseOffset = 0;
+        public float SpeechSkillImproveMult = 2;
+        public float SpeechSkillImproveOffset = 0;
+    }
+}
